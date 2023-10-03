4c9256f1c0b815f7b33368806216952ddc77dfcb
Mykhailo <74735041+mikhail2574@users.noreply.github.com>
2023-10-03 10:46:36 +0200

    Update itemSlice.js

--- src/redux/counter/itemSlice.js
+++ src/redux/counter/itemSlice.js
@@ -12,23 +12,12 @@ const itemSlice = createSlice({
     },
     addItem: (state, action) => {
       state.allItems.push(action.payload);
-      state.filteredItems.push(action.payload);
     },
     removeItem: (state, action) => {
       state.allItems = state.allItems.filter(
         contact => contact.id !== action.payload
       );
-      state.filteredItems = state.filteredItems.filter(
-        contact => contact.id !== action.payload
-      );
     },
-    // filterItems: state => {
-    //   if (state.q) {
-    //     return state.allItems.filter(contact => contact.name.includes(state.q));
-    //   } else {
-    //     return state.allItems;
-    //   }
-    // },
   },
 });
 
